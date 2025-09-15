69619359ad85bba713bdca46f0dd38aa655db4af
Abigail Santiago <abigail.santiago@northwestschool.org>
2025-09-15 09:02:12 -0700

    Update bakery.py

--- bakery.py
+++ bakery.py
@@ -6,18 +6,18 @@
 # Enter sales for bread type 2: 3 4 2 5
 # Enter sales for bread type 3: 9 6 7 8 
 
+# Note that the sales for bread type input comes in as a list of strings. 
+
+
 # Expected Output:
 # Total sales for each bread: [26, 14, 30]
 # Average daily sales: [5.66, 5.66, 5.66, 6.33]
 
-#If you feel overwhelmed by the problem, try writing down 1. the information you need, 2. the 2D array, and 3. the steps you need to take to solve the problem. 
+# If you feel overwhelmed by the problem, try writing down 1. the information you need, 2. a representation og the 2D array, and
+# 3. the steps you need to take to solve the problem. 
 
 def bakery():
 
-
-
-
-print(bakery())
 
 
   
